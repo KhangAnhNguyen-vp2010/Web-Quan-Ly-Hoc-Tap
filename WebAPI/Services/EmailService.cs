ed420dee179a15fe01507c1d6a17d7a8dec5d83e
Anh Khang <taicuoi664@gmail.com>
2025-04-21 22:23:07 +0700

    update backend: forgot password - send otp email

---
+++ WebAPI/Services/EmailService.cs
@@ -0,0 +1,30 @@
+﻿using MailKit.Net.Smtp;
+using MimeKit;
+using WebAPI.Services.Interfaces;
+namespace WebAPI.Services
+{
+    public class EmailService : IEmailService
+    {
+        private readonly IConfiguration _config;
+
+        public EmailService(IConfiguration config)
+        {
+            _config = config;
+        }
+
+        public async Task SendEmailAsync(string to, string subject, string body)
+        {
+            var email = new MimeMessage();
+            email.From.Add(MailboxAddress.Parse(_config["EmailSettings:From"]));
+            email.To.Add(MailboxAddress.Parse(to));
+            email.Subject = subject;
+            email.Body = new TextPart(MimeKit.Text.TextFormat.Plain) { Text = body };
+
+            using var smtp = new SmtpClient();
+            await smtp.ConnectAsync(_config["EmailSettings:SmtpServer"], 587, MailKit.Security.SecureSocketOptions.StartTls);
+            await smtp.AuthenticateAsync(_config["EmailSettings:Username"], _config["EmailSettings:Password"]);
+            await smtp.SendAsync(email);
+            await smtp.DisconnectAsync(true);
+        }
+    }
+}
---
+++ WebAPI/Services/Interfaces/IEmailService.cs
@@ -0,0 +1,7 @@
+﻿namespace WebAPI.Services.Interfaces
+{
+    public interface IEmailService
+    {
+        Task SendEmailAsync(string to, string subject, string body);
+    }
+}
