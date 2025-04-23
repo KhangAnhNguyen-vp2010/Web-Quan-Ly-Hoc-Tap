a551d5e14ee43735c66d17cba9781a7bcf2f5938
Anh Khang <taicuoi664@gmail.com>
2025-04-23 22:10:24 +0700

    update front-end: interface Courses - edit Course

---
+++ react-app/src/components/Interfaces/Instructors/Courses/EditCourse.jsx
@@ -0,0 +1,112 @@
+import { useState } from "react";
+import axios from "axios";
+import styles from "../../../../assets/css/Instructor/Courses/EditCourse.module.css";
+import { toast } from "react-toastify";
+import { useCourse } from "../../../../contexts/CourseContext";
+
+function EditCourse({ onClose, courseObject }) {
+  const [formData, setFormData] = useState({
+    courseName: courseObject.courseName,
+    courseDescription: courseObject.description,
+  });
+  const [selectedImage, setSelectedImage] = useState(null);
+
+  const { setLoad } = useCourse();
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    try {
+      await axios.put(
+        `https://localhost:7233/api/courses/${courseObject.courseId}`,
+        {
+          courseName: formData.courseName,
+          description: formData.courseDescription,
+        }
+      );
+    } catch (error) {
+      console.error("Error updating course:", error);
+      return;
+    }
+    onClose();
+    const form = new FormData();
+    if (selectedImage) {
+      form.append("file", selectedImage);
+    } else {
+      const fullPath = courseObject.img;
+      const fullFileName = fullPath.split("/").pop();
+      const originalName = fullFileName.split("_").pop();
+      form.append("file", originalName);
+    }
+    try {
+      await axios.post(
+        `https://localhost:7233/api/Courses/upload/${courseObject.courseId}`,
+        form
+      );
+    } catch (error) {
+      console.error("Error uploading image:", error);
+    }
+    setLoad(true);
+  };
+
+  return (
+    <div className={styles.overlay}>
+      <div className={styles["edit-course-container"]}>
+        <h2>Edit Course</h2>
+
+        <img
+          src={
+            selectedImage
+              ? URL.createObjectURL(selectedImage)
+              : `https://localhost:7233${courseObject.img}`
+          }
+          alt={courseObject.courseName}
+          className={styles["edit-img"]}
+        />
+
+        <form onSubmit={handleSubmit}>
+          <div className={styles["form-group"]}>
+            <label htmlFor="courseName">Course Name:</label>
+            <input
+              type="text"
+              id="courseName"
+              value={formData.courseName}
+              onChange={(e) =>
+                setFormData({ ...formData, courseName: e.target.value })
+              }
+            />
+          </div>
+
+          <div>
+            <label htmlFor="courseDescription">Course Description:</label>
+            <textarea
+              id="courseDescription"
+              value={formData.courseDescription}
+              onChange={(e) =>
+                setFormData({ ...formData, courseDescription: e.target.value })
+              }
+            ></textarea>
+          </div>
+          <div className={styles["form-group"]}>
+            <label htmlFor="courseImage">Course Image:</label>
+            <input
+              type="file"
+              id="courseImage"
+              accept="image/*"
+              onChange={(e) => setSelectedImage(e.target.files[0])}
+            />
+          </div>
+          <div className={styles["form-actions"]}>
+            <button type="button" onClick={onClose} className={styles.cancel}>
+              Cancel
+            </button>
+            <button type="submit" className={styles.submit}>
+              Save Changes
+            </button>
+          </div>
+        </form>
+      </div>
+    </div>
+  );
+}
+
+export default EditCourse;
