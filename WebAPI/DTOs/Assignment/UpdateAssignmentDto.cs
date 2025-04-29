ed9c9475d2489ab38993765257981720176c25d4
Anh Khang <taicuoi664@gmail.com>
2025-04-29 16:13:21 +0700

    feat(api): Add API to create and update assignments
    - Created API endpoint to add a new assignment
    - Created API endpoint to update an existing assignment
    - Implemented input validation for assignment data

---
+++ WebAPI/DTOs/Assignment/UpdateAssignmentDto.cs
@@ -0,0 +1,9 @@
+﻿namespace WebAPI.DTOs.Assignment
+{
+    public class UpdateAssignmentDto
+    {
+        public string? AssignmentName { get; set; }
+        public DateOnly? DueDate { get; set; }
+        public string? AssignmentContent { get; set; }
+    }
+}
